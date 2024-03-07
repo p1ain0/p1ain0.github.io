81b9057ccb5506b4d30173ad2ca4203e3f85bf7a
p1ain0 <assdfgzxcv4@gmail.com>
2024-03-07 22:43:16 +0800

    submit a artical

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709398259',resources: [
+    cacheName: 'chirpy-1709822258',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
