6c28a16313e183345d13bbb59c339eabdd3fa6eb
p1ain0 <assdfgzxcv4@gmail.com>
2024-03-10 10:37:09 +0800

    change email config

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1709911657',resources: [
+    cacheName: 'chirpy-1710038150',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
