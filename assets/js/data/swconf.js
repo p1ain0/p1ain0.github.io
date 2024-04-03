f6972876313f89847808992fb616c9a93ed84849
p1ain0 <assdfgzxcv4@gmail.com>
2024-04-03 21:19:31 +0800

    add a post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712149912',resources: [
+    cacheName: 'chirpy-1712150244',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
